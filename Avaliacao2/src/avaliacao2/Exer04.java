ad985dde40ad011c77415d4f8cf0b142fa94f5a4
Rone Silva <>
2020-06-02 20:27:42 -0300

    Commit do exercício 04.

--- Avaliacao2/src/avaliacao2/Exer04.java
+++ Avaliacao2/src/avaliacao2/Exer04.java
@@ -16,11 +16,29 @@ package avaliacao2;
  * @author Leonardo Maia Silva
  * @author Rone Silva
  */
-
 public class Exer04 {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        //chamada do método e armazenamento do retorno na variável retorno
+        int retorno = contarX("hixx", 'x');
+        //impressão do resultado
+        System.out.println("Quantidade: "+retorno);
     }
-    
+
+    private static int contarX(String texto, char letraProcurada) 
+    {
+        //se o texto for vazio, retorna zero.
+        if (texto.isEmpty()) 
+            return 0;
+        
+        //se não, se a primeira posição do texto for a letra procurada
+        //então faz uma chamada recursiva com o incremento +1
+        else 
+            if (texto.charAt(0) == letraProcurada)
+                return contarX(texto.substring(1), letraProcurada) + 1;
+        //se não, faz a chamada recursiva sem incremento
+            else return contarX(texto.substring(1), letraProcurada);
+    }
+
 }
